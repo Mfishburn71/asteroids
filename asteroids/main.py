8f2851a51c3dfece499c911ea8a7e8d34907405d
Mark <mfishburn@protonmail.com>
2025-02-08 11:10:43 -0500

    Completed asteroids

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,22 +1,61 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
-pygame.init
+from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     print("Starting asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Create player
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0)) # Fill screen
+        screen.fill("black") # Fill screen
+        updatable.update(dt) # Update player
+
+        for asteroid in asteroids: # Check for collisions
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
+                    break
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip() # Update screen
+        dt = Clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
